1fed8b9ca2999304eaceffffdf3047d4cd3c126a
Miles Saunders <saundersmiles@gmail.com>
2022-09-18 13:58:54 +0100

    tidy up

--- build-3rd_party-Desktop_Qt_5_15_2_MSVC2019_64bit/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Visual Studio 16 2019" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/Qt/5.15.2/msvc2019_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/Qt/5.15.2/msvc2019_64" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.32.31326/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.32.31326/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
