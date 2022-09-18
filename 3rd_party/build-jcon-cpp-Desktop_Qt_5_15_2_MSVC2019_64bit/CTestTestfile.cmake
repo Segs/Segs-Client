b942e263fdfc34b81876508ae199bdab428099df
Miles Saunders <saundersmiles@gmail.com>
2022-09-18 13:57:06 +0100

    further changes to make STATIC build instead of SHARED for jcon lib

---
+++ 3rd_party/build-jcon-cpp-Desktop_Qt_5_15_2_MSVC2019_64bit/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Code/Segs-Client/3rd_party/jcon-cpp
+# Build directory: C:/Code/Segs-Client/3rd_party/build-jcon-cpp-Desktop_Qt_5_15_2_MSVC2019_64bit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("jcon")
