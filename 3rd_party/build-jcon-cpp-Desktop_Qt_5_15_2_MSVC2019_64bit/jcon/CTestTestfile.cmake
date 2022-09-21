816daa1e9089812d415e481d1b9e02ff36a62e3c
Miles Saunders <saundersmiles@gmail.com>
2022-09-21 21:12:24 +0100

    removed jcon build files

--- 3rd_party/build-jcon-cpp-Desktop_Qt_5_15_2_MSVC2019_64bit/jcon/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Code/Segs-Client/3rd_party/jcon-cpp/jcon
-# Build directory: C:/Code/Segs-Client/3rd_party/build-jcon-cpp-Desktop_Qt_5_15_2_MSVC2019_64bit/jcon
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
